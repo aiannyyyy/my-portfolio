1f56db546f4a28c8b461610481fa0bbf86d6a4e2
John Adrian M. Ticatic <john.ticatic@gmail.com>
2025-12-22 08:11:58 +0800

    adding snowflake on my portfolio website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
+import Snowfall from "./components/Snowfall";
 
 export default function App() {
   return (
     <>
+      <Snowfall />
       <Navbar />
       <Content />
     </>
