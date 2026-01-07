982ca8081b5e82aa9e3d69dfb0d91fc9e3e2f580
John Adrian M. Ticatic <john.ticatic@gmail.com>
2026-01-07 16:55:31 +0800

    remove snowfall on my portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
-import Snowfall from "./components/Snowfall";
 
 export default function App() {
   return (
     <>
-      <Snowfall />
       <Navbar />
       <Content />
     </>
